ee2e6275342e72bbf46080bd77c5dc36dfc9b0c3
Travis Fields <travis@puppetlabs.com>
2014-09-10 11:21:58 -0700

    Fix issue with puppet_module_install, removed and using updated method from beaker core copy_module_to

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -25,8 +25,8 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'haproxy')
     hosts.each do |host|
+      copy_module_to(host, :source => proj_root, :module_name => 'haproxy')
       on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module','install','puppetlabs-concat'), { :acceptable_exit_codes => [0,1] }
       if fact('osfamily') == 'RedHat'
