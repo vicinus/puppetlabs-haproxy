0bd3487bda4f85a58046e0ed0a04fe58d14ec01e
Yehuda Katz <ykatz@cs.umd.edu>
2023-10-17 15:20:38 -0400

    Add tests for defaults merge

--- spec/defines/defaults_spec.rb
+++ spec/defines/defaults_spec.rb
@@ -37,4 +37,38 @@ describe 'haproxy::defaults' do
       )
     }
   end
+
+  context 'with merge defaults true' do
+    let(:params) do
+      {
+        options: { 'balance' => 'roundrobin' },
+        merge_options: true
+      }
+    end
+
+    defaults_output = <<~EXPECTEDOUTPUT
+
+
+      defaults test
+        balance roundrobin
+        log global
+        maxconn 8000
+        option redispatch
+        retries 3
+        stats enable
+        timeout http-request 10s
+        timeout queue 1m
+        timeout connect 10s
+        timeout client 1m
+        timeout server 1m
+        timeout check 10s
+    EXPECTEDOUTPUT
+    it {
+      is_expected.to contain_concat__fragment('haproxy-test_defaults_block').with(
+        'order' => '25-test',
+        'target' => '/tmp/haproxy.cfg',
+        'content' => defaults_output,
+      )
+    }
+  end
 end
