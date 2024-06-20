8fb1ce51a8d6eaa0aca5c21379d97c6cd4c6ca98
Reinhard Vicinus <r.vicinus@metaways.de>
2024-06-20 17:24:10 +0200

    allow ip ranges on ports

--- types/ports.pp
+++ types/ports.pp
@@ -1,3 +1,3 @@
 # @summary Port or list of ports for haproxy. Supports `,` seperated list of ports also.
 #
-type Haproxy::Ports = Variant[Array[Variant[Pattern[/^[0-9]+$/],Stdlib::Port],0], Pattern[/^[0-9,]+$/], Stdlib::Port]
+type Haproxy::Ports = Variant[Array[Variant[Pattern[/^[0-9]+$/],Stdlib::Port,Array[Stdlib::Port, 2, 2]],0], Pattern[/^[0-9,]+$/], Stdlib::Port]
